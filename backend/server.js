8fc7da734e62acc8517cd546e3ed061943739ffa
je215 <144349125+je215@users.noreply.github.com>
2023-09-24 10:19:29 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const cors = require('cors');
 const mongoose = require("mongoose");
 const port = 3001;
-const routes = require("./routes");
+const routes = require("./routes/todos"); 
+const bodyParser = require("body-parser");
 
 main().catch((err) => console.log(err));
 
@@ -12,8 +13,14 @@ async function main() {
     useNewUrlParser: true,
   });
   const app = express();
+  
+  // Use CORS middleware with the main app instance
   app.use(cors());
+  
+  // Use body-parser middleware for parsing JSON request bodies
   app.use(express.json());
+
+  // Use your API routes from the routes router
   app.use("/api", routes);
 
   app.listen(port, () => {
